59ab97493a90092cbcdbcb45132ddf7751c0fa05
Jerald Austero <jerald.austero@tawk.to>
2022-01-05 21:36:18 +0800

    Call Tawk_API with parent object window

--- src/utils/widget.js
+++ src/utils/widget.js
@@ -1,4 +1,3 @@
-/* global Tawk_API */
 /* eslint-disable no-unused-vars */
 
 function loadScript({propertyId = '', widgetId = '', embedId = ''}) {
@@ -13,7 +12,7 @@ function loadScript({propertyId = '', widgetId = '', embedId = ''}) {
 			document.body.appendChild(element);
 		}
 
-		Tawk_API.embedded = embedId;
+		window.Tawk_API.embedded = embedId;
 	}
 
 	const script = document.createElement('script');
