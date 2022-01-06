d6b772cfbac11a315bd8accf2aadf50ba9071c7b
Jerald Austero <jerald.austero@tawk.to>
2022-01-07 02:37:51 +0800

    Ran eslint and fix

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
 	moduleFileExtensions : ['js', 'jsx', 'json', 'vue'],
 	transform : {
-		'^.+\\.vue$': 'vue-jest',
-		'.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-		'^.+\\.jsx?$': 'babel-jest',
+		'^.+\\.vue$' : 'vue-jest',
+		'.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$' : 'jest-transform-stub',
+		'^.+\\.jsx?$' : 'babel-jest',
 	},
 	transformIgnorePatterns : [
 		'/node_modules/',
 	],
 	moduleNameMapper : {
-		'^@/(.*)$': '<rootDir>/src/$1',
+		'^@/(.*)$' : '<rootDir>/src/$1',
 	},
 	snapshotSerializers : [
 		'jest-serializer-vue',
